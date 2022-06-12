cfb43e118a24967b2d360fe94f2a132aa33f7f48
mauro-balades <mauro.balades@tutanota.com>
2022-06-12 21:05:54 +0200

    added CLI options parsing

--- src/cli.ts
+++ src/cli.ts
@@ -1,2 +1,18 @@
+import { Command, Option } from "commander";
+import consts from "./consts";
 
+const program = new Command();
+program
+    .version(consts.VERSION)
+    .requiredOption('-p, --port <port>', 'Internal HTTP server port')
+    .addOption(new Option('-h, --host <host>', 'Upstream server providing forwarding').default(consts.DEFAULT_HOST).env("PORT"))
 
+const main = (): void => {
+    program.parse(process.argv);
+    const options = program.opts();
+
+    
+
+}
+
+main()
---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+
+export default {
+    VERSION: '1.0.0',
+    DEFAULT_HOST: "vcap.me", // For now
+};
